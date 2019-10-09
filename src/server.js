4540d3e0dcd83d4ade21d99f34bd1a5dc17d5e6c
Eduardo Freitas <edusf8700@gmail.com>
2019-10-09 03:00:34 -0300

    Criando a rota de login

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes  = require('./routes');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://edusf8700:153426edu@aircnc-xhnic.mongodb.net/aircnc?retryWrites=true&w=majority',{
+    useNewUrlParser : true,
+    useUnifiedTopology :true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
