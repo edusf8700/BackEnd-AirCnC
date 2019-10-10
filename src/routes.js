af1029226fdd6eed41863592c48884c71dc7573f
Eduardo Freitas <edusf8700@gmail.com>
2019-10-10 02:58:43 -0300

    Criado a rota de listagem de spots por usuário

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -11,5 +12,6 @@ const upload = multer(uploadConfig);
 routes.post('/sessions', SessionController.store);
 routes.post('/spots',upload.single('thumbnail'), SpotController.store);
 routes.get('/spots', SpotController.index);
+routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;
