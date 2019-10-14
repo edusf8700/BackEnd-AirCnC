2a40f6c07aee34034eb0497cc0c75faa14233a8f
Eduardo Freitas <edusf8700@gmail.com>
2019-10-14 04:00:04 -0300

    Criado rota estática para arquivos

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes  = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://edusf8700:153426edu@aircnc-xhnic.mongodb.net/air
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
